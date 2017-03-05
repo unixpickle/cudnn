e20e4c695aba3a1d537fcae300e57edffb72e722
Alex Nichol <unixpickle@gmail.com>
2017-03-05 18:30:22 -0500

    finished tensor desc API draft

--- tensor.go
+++ tensor.go
@@ -9,6 +9,9 @@ const cudnnTensorFormat_t goCudnnNHWC = CUDNN_TENSOR_NHWC;
 const cudnnDataType_t goCudnnFloat = CUDNN_DATA_FLOAT;
 const cudnnDataType_t goCudnnDouble = CUDNN_DATA_DOUBLE;
 const cudnnDataType_t goCudnnHalf = CUDNN_DATA_HALF;
+
+const size_t goCudnnIntSize = sizeof(int);
+const int * goCudnnNullArray = NULL;
 */
 import "C"
 
@@ -46,6 +49,19 @@ const (
 	Double
 	Half
 )
+
+func newDataTypeC(val C.cudnnDataType_t) DataType {
+	switch val {
+	case C.goCudnnFloat:
+		return Float
+	case C.goCudnnDouble:
+		return Double
+	case C.goCudnnHalf:
+		return Half
+	default:
+		panic("unable to create DataType")
+	}
+}
 
 func (d DataType) cValue() C.cudnnDataType_t {
 	switch d {
@@ -91,4 +107,85 @@ func (t *TensorDesc) Set4D(format TensorFormat, dataType DataType, n, c, h, w in
 	status := C.cudnnSetTensor4dDescriptor(t.desc, format.cValue(), dataType.cValue(),
 		safeIntToC(n), safeIntToC(c), safeIntToC(h), safeIntToC(w))
 	return newError("cudnnSetTensor4dDescriptor", status)
+}
+
+// Set4DEx is like Set4D, but with custom strides.
+//
+// This should be called from the cuda.Context.
+func (t *TensorDesc) Set4DEx(dataType DataType, n, c, h, w, nStride, cStride,
+	hStride, wStride int) error {
+	status := C.cudnnSetTensor4dDescriptorEx(t.desc, dataType.cValue(),
+		safeIntToC(n), safeIntToC(c), safeIntToC(h), safeIntToC(w),
+		safeIntToC(nStride), safeIntToC(cStride), safeIntToC(hStride),
+		safeIntToC(wStride))
+	return newError("cudnnSetTensor4dDescriptorEx", status)
+}
+
+// Get4D gets the parameters of the 4D tensor.
+//
+// This should be called from the cuda.Context.
+func (t *TensorDesc) Get4D() (dataType DataType, n, c, h, w, nStride, cStride,
+	hStride, wStride int, err error) {
+	var cn, cc, ch, cw, cnStride, ccStride, chStride, cwStride C.int
+	var cType C.cudnnDataType_t
+	status := C.cudnnGetTensor4dDescriptor(t.desc, &cType, &cn, &cc, &ch, &cw,
+		&cnStride, &ccStride, &chStride, &cwStride)
+	err = newError("cudnnGetTensor4dDescriptor", status)
+	dataType = newDataTypeC(cType)
+	n, c, h, w = int(cn), int(cc), int(ch), int(cw)
+	nStride, cStride, hStride, wStride = int(cnStride), int(ccStride), int(chStride),
+		int(cwStride)
+	return
+}
+
+// Set sets variable-length tensor information.
+//
+// This should be called from the cuda.Context.
+func (t *TensorDesc) Set(dataType DataType, dims, strides []int) error {
+	if len(dims) != len(strides) {
+		panic("dims and stride length mismatch")
+	}
+	cDims := make([]C.int, len(dims))
+	cStrides := make([]C.int, len(strides))
+	for i, x := range dims {
+		cDims[i] = safeIntToC(x)
+	}
+	for i, x := range strides {
+		cStrides[i] = safeIntToC(x)
+	}
+	status := C.cudnnSetTensorNdDescriptor(t.desc, dataType.cValue(),
+		safeIntToC(len(dims)), (*C.int)(&cDims[0]), (*C.int)(&cStrides[0]))
+	return newError("cudnnSetTensorNdDescriptor", status)
+}
+
+// Get gets the variable-length tensor information.
+//
+// This should be called from the cuda.Context.
+func (t *TensorDesc) Get() (dataType DataType, dims, strides []int, err error) {
+	var cType C.cudnnDataType_t
+	var nDims C.int
+	status := C.cudnnGetTensorNdDescriptor(t.desc, 0, &cType, &nDims,
+		C.goCudnnNullArray, C.goCudnnNullArray)
+	err = newError("cudnnGetTensorNdDescriptor", status)
+	if err != nil {
+		return
+	}
+
+	outDims := make([]C.int, int(nDims))
+	outStrides := make([]C.int, int(nDims))
+	status = C.cudnnGetTensorNdDescriptor(t.desc, nDims, &cType, &nDims,
+		(*C.int)(&outDims[0]), (*C.int)(&outStrides[0]))
+	err = newError("cudnnGetTensorNdDescriptor", status)
+	if err != nil {
+		return
+	}
+
+	dataType = newDataTypeC(cType)
+	dims = make([]int, len(outDims))
+	strides = make([]int, len(outStrides))
+	for i := range dims {
+		dims[i] = int(outDims[i])
+		strides[i] = int(outStrides[i])
+	}
+	return
 }
